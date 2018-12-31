45014de47d48c96dea32140382f4d49a82204374
Evgeny Karpovsky <ekarpovsky@gmail.com>
2018-12-31 17:10:36 +0200

    change route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,7 +5,7 @@ import * as apiController from '../controllers/api';
 
 const setRoutes = (app: Application) => {
   app.post('/account/signup', apiController.postSignup);
-  app.get('/users', apiController.getList);
+  app.get('/account/list', apiController.getList);
 };
 
 export default setRoutes;
