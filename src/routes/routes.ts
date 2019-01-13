54c19b27071a582ab6e1f298d7ccd9d2af2e5d25
Evgeny Karpovsky <ekarpovsky@gmail.com>
2019-01-13 17:39:34 +0200

    Add test environment and home test

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,8 +2,10 @@ import { Application } from 'express';
 
 // Controllers (route handlers)
 import * as apiController from '../controllers/api';
+import * as homeController from '../controllers/home';
 
 const setRoutes = (app: Application) => {
+  app.get('/', homeController.getHome);
   app.post('/account/signup', apiController.postSignup);
   app.get('/account/list', apiController.getList);
   app.get('/account', apiController.getAccount);
